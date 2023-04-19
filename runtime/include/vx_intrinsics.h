ce1384fb4094d76100ca0b48a5ab5a965c47a5ec
xXGunner989x <williamkaye94@gmail.com>
2023-04-19 16:16:59 -0400

    lets see if ths works

--- runtime/include/vx_intrinsics.h
+++ runtime/include/vx_intrinsics.h
@@ -212,7 +212,7 @@ inline int vx_rori(unsigned rs1, int imm) {
     // op: 0x13, func3: 0x5, shamt & func7 = 0x18
     int result;
     imm = (0x18 << 5) | (imm & 0b11111); // constructing imm12
-    asm volatile (".insn i 0x13, 0x5, %0, %1, %2" : "=r"(result) : "r"(rs1), "i"(imm));
+    asm volatile (".insn i 0x13, 0x5, %0, %1, %2" : "=r"(result) : "r"(rs1), "r"(imm));
     return result;  
 }
 
